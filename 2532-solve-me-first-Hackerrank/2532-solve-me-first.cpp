587d4629d7841ac1c745924512ce510df4390836
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-06-29 11:53:15 +0530

    Added solution - LeetHub

---
+++ 2532-solve-me-first-Hackerrank/2532-solve-me-first.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int solveMeFirst(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    cout << solveMeFirst(a, b);
+
+    return 0;
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
