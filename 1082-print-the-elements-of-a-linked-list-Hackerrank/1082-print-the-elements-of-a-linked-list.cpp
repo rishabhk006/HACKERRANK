7672f33765cbc19a65fbfb5128b57c4e97804380
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-07-03 11:57:54 +0530

    Added solution - LeetHub

---
+++ 1082-print-the-elements-of-a-linked-list-Hackerrank/1082-print-the-elements-of-a-linked-list.cpp
@@ -0,0 +1,26 @@
+
+
+// Complete the printLinkedList function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+void printLinkedList(SinglyLinkedListNode* head) {
+    SinglyLinkedListNode* current = head;
+    while (current != nullptr) {
+        cout << current->data << "\n";
+        current = current->next;
+    }
+}
+
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
