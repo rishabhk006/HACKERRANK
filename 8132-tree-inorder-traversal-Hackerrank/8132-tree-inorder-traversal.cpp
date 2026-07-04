5cccd5017b927ce87e903d9279e58c05d0f4257f
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-07-04 18:17:11 +0530

    Added solution - LeetHub

---
+++ 8132-tree-inorder-traversal-Hackerrank/8132-tree-inorder-traversal.cpp
@@ -0,0 +1,32 @@
+
+/* you only have to complete the function given below.  
+Node is defined as  
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+*/
+
+    void inOrder(Node *root) {
+        if (root == nullptr) {
+        return;
+    }
+    inOrder(root->left);
+    cout << root->data << " ";
+    inOrder(root->right);
+    }
+
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
