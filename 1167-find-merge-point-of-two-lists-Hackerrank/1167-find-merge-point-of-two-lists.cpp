47a0994962610cfbd929e63c66aac2e2cbea97b4
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-07-11 18:41:17 +0530

    Added solution - LeetHub

---
+++ 1167-find-merge-point-of-two-lists-Hackerrank/1167-find-merge-point-of-two-lists.cpp
@@ -0,0 +1,31 @@
+
+
+// Complete the findMergeNode function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+int findMergeNode(SinglyLinkedListNode* head1, SinglyLinkedListNode* head2) {
+
+ SinglyLinkedListNode *p1 = head1;
+    SinglyLinkedListNode *p2 = head2;
+
+    while (p1 != p2) {
+        p1 = (p1 == NULL) ? head2 : p1->next;
+        p2 = (p2 == NULL) ? head1 : p2->next;
+    }
+
+    return p1->data;
+}
+
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
